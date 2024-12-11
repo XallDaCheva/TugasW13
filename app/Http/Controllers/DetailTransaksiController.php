f68364314e046557b503feeeece49e80d07ae8f5
RD FAISAL <rdmfaisalrj@student.telkomuniversity.ac.id>
2024-12-11 18:04:21 +0700

    migration

--- app/Http/Controllers/DetailTransaksiController.php
+++ app/Http/Controllers/DetailTransaksiController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class DetailTransaksiController extends Controller
 {
-    //
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('transaction_id');
+            $table->integer('quantity');
+            $table->decimal('price_at_sale', 10, 2);
+            $table->string('product_id');
+
+            $table->foreign('transaction_id')->references('id')->on('transactions')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
 }
--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class TransaksiController extends Controller
 {
-    //
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->timestamp('date');
+            $table->decimal('total_amount', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
 }
